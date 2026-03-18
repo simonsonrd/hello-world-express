b81909482e7738a203ee200a85abe9ac94caa3a3
simonsonrd <ryandsimonson@gmail.com>
2026-03-18 08:18:18 -0500

    Intentional failing test for CI screenshot

--- http-server.test.js
+++ http-server.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should display "Hello World" on the main page', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toContain('Hello World');
+    expect(response.text).toContain('Goodbye World');
   });
 });
